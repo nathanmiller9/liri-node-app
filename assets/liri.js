890437abef4d8c99ac3255e1919c7939c204030c
Nate Miller <nathanmiller9@yahoo.com>
2017-04-08 10:22:20 -0700

    dealt with spotify deprecation/callback issue

--- assets/liri.js
+++ assets/liri.js
@@ -46,11 +46,31 @@ if (process.argv[2] === "spotify-this-song") {
                 console.log("Preview URL: " + data.tracks.items[i]["preview_url"]);
                 console.log("Name of Song: " + data.tracks.items[i]["name"]);
                 console.log("Artist: " + data.tracks.items[i].artists[0].name);
-                fs.appendFile("../log.txt", "--------------------");
-                fs.appendFile("../log.txt", "Album Name: " + data.tracks.items[i].album["name"]);
-                fs.appendFile("../log.txt", "Preview URL: " + data.tracks.items[i]["preview_url"]);
-                fs.appendFile("../log.txt", "Name of Song: " + data.tracks.items[i]["name"]);
-                fs.appendFile("../log.txt", "Artist: " + data.tracks.items[i].artists[0].name);
+                fs.appendFile("../log.txt", "--------------------", function(err) {
+                            if (err) {
+                                console.log(err);
+                            }
+                        });
+                        fs.appendFile("../log.txt", "Album Name: " + data.tracks.items[i].album["name"], function(err) {
+                            if (err) {
+                                console.log(err);
+                            }
+                        });
+                        fs.appendFile("../log.txt", "Preview URL: " + data.tracks.items[i]["preview_url"], function(err) {
+                            if (err) {
+                                console.log(err);
+                            }
+                        });
+                        fs.appendFile("../log.txt", "Name of Song: " + data.tracks.items[i]["name"], function(err) {
+                            if (err) {
+                                console.log(err);
+                            }
+                        });
+                        fs.appendFile("../log.txt", "Artist: " + data.tracks.items[i].artists[0].name, function(err) {
+                            if (err) {
+                                console.log(err);
+                            }
+                        });
             }
         }
     });
